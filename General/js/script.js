5fe0b9533da5b16bd66124a8d7d4ba6cdecff1e0
Konrad Iwanczewski <konrad.iwanczewski@student.pk.edu.pl>
2025-07-24 19:53:44 +0200

    commit

--- General/js/script.js
+++ General/js/script.js
@@ -1,3 +1,5 @@
 ﻿
-document.addEventListener("DOMContentLoaded", () => {console.log("Siemanko");
+//Wstęp i guess
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Siemanko");
 });

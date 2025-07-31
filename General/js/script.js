31831c4516876cbc65c2f5b8a65aef571dbe1647
Konrad Iwanczewski <konrad.iwanczewski@student.pk.edu.pl>
2025-07-31 20:15:29 +0200

    commit na dziś

--- General/js/script.js
+++ General/js/script.js
@@ -1,5 +1,14 @@
-﻿
-//Wstęp i guess
+﻿//Wstęp i guess
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Siemanko");
+});
+
+
+const audio = document.getElementById("audio");
+audio.volume = 0.2; // Ustawienie głośności
+
+const muteButton = document.getElementById("muteButton");
+muteButton.addEventListener("click", function () {
+    audio.muted = !audio.muted;
+    muteButton.textContent = audio.muted ? "🔇" : "🔊";
 });
